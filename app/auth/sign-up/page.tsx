3523f8a117339f4d9519047503f47e144b010680
Nihan <nihanmubashshir@gmail.com>
2023-12-09 12:25:44 +0600

    feat: changed project structure to resemble modern nextjs 13 app

---
+++ app/auth/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <div className="container max-w-[1440px] grid grid-cols-12 gap-8 min-h-screen mx-auto">
+      <h1>hello world</h1>
+    </div>
+  );
+}
