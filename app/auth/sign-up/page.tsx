3f32a79bb9f33b3e4267cbbf783918b3746b4280
Nihan <nihanmubashshir@gmail.com>
2023-12-09 12:41:34 +0600

    feat: added shadcn

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,6 +1,6 @@
 export default function SignUp() {
   return (
-    <div className="container max-w-[1440px] grid grid-cols-12 gap-8 min-h-screen mx-auto">
+    <div className="container max-w-[1440px] grid grid-cols-12 px-5 md:px-8 gap-8 min-h-screen mx-auto">
       <h1>hello world</h1>
     </div>
   );
