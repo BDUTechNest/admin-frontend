1d8142c205d27e7aa9ed9604ee76a57ba3ef9dda
Nihan <nihanmubashshir@gmail.com>
2023-12-11 18:16:32 +0600

    feat: added basic structure

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,7 +1,11 @@
+"use-client";
+
+import SignUpForm from "@/components/auth/sign-up-form";
+
 export default function SignUp() {
   return (
     <div className="container max-w-[1440px] grid grid-cols-12 px-5 md:px-8 gap-8 min-h-screen mx-auto">
-      <h1>hello world</h1>
+      <SignUpForm></SignUpForm>
     </div>
   );
 }
