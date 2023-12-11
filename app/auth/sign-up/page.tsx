4bb1b27ebc880e3318c9728f968e8528c4471f51
Nihan <nihanmubashshir@gmail.com>
2023-12-11 19:01:53 +0600

    feat: sign up form layout

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,10 +1,8 @@
-"use-client";
-
 import SignUpForm from "@/components/auth/sign-up-form";
 
 export default function SignUp() {
   return (
-    <div className="container max-w-[1440px] grid grid-cols-12 px-5 md:px-8 gap-8 min-h-screen mx-auto">
+    <div className="container max-w-[1440px] border-[1px]   grid grid-cols-12 px-5 md:px-8 gap-8 min-h-screen mx-auto">
       <SignUpForm></SignUpForm>
     </div>
   );
--- components/auth/sign-up-form.tsx
+++ components/auth/sign-up-form.tsx
@@ -1,5 +1,11 @@
 "use-client";
 
 export default function SignUpForm() {
-  return <div></div>;
+  return (
+    <div className="bg-white  h-[70%] cols-start py-5 px-6 col-end-7 self-center rounded-sm shadow-md col-span-4 max-w-[446px]">
+      <h1 className="text-center font-bold text-[30px] text-green-500 ">
+        Sign Up
+      </h1>
+    </div>
+  );
 }
